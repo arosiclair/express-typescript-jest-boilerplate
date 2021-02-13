7458311a91b0fac8a110ce43549501f616faaa04
Andrew Rosiclair <arosiclair@gmail.com>
2021-02-12 22:20:30 -0500

    simplified jest config

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,7 @@
 module.exports = {
   transform: {
-    "^.+\\.tsx?$": "ts-jest"
+    '^.+\\.tsx?$': 'ts-jest',
   },
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   collectCoverage: true,
-  testEnvironment: "node",
-  coverageReporters: ["json", "lcov", "text", "clover"] // "text-summary"
+  testEnvironment: 'node',
 };
